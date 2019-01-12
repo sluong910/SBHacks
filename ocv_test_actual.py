b4d771513d509fdfc93cf3a93bea549351146fee
Minh Hua <minhhua12345@gmail.com>
2019-01-12 08:53:33 -0800

    final

--- ocv_test_actual.py
+++ ocv_test_actual.py
@@ -4,6 +4,7 @@ from googletrans import Translator
 import re
 from PIL import Image
 import numpy as np
+from PIL import Image, ImageEnhance, ImageFilter
 
 # config
 pytesseract.pytesseract.tesseract_cmd = 'C:/Program Files (x86)/Tesseract-OCR/tesseract.exe'
@@ -11,17 +12,20 @@ translator = Translator()
 
 def parse_img(img, lang):
 	try:
-	    f = open(img, 'rb').read()
-	    nparr = np.frombuffer(f, np.uint8)
-	    img_np = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
-	    img = Image.fromarray(img_np)
-	    text = pytesseract.image_to_string(img, lang=lang)
-	    return clean_text(text)
+		f = open(img, 'rb').read()
+		nparr = np.frombuffer(f, np.uint8)
+		img_np = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+		img = Image.fromarray(img_np)
+		im = img.convert('L')                             # grayscale
+		im = im.filter(ImageFilter.MedianFilter())       # a little blur
+		img = im.point(lambda x: 0 if x < 140 else 255)   # threshold (binarize)
+		text = pytesseract.image_to_string(img, lang=lang)
+		return clean_text(text)
 	except Exception as e:
 		return str(e)
 
 def translate(text, og, target):
-    translated = translator.translate(text, src=og, dest=target)
+    translated = translator.translate(text, src=og, dest=target).text
     return translated
 
 def generate_vocab(text, og):
@@ -33,32 +37,30 @@ def generate_vocab(text, og):
                 pronun_list = [re.sub("\W+", '', pron).lower() for pron in pronun_list_old]
                 # print(pronun_list)
                 while i < len(text):
-                        translator = Translator()
-                if translator.translate(text[i], src=og, dest=og).pronunciation.lower() == pronun_list[k].lower():
-                        dic[text[i]] = pronun_list[k] + " | " + translate(text[i], og, "en").text
-                        k += 1
-                        i += 1
-                else:
-                        word = translator.translate(text[i], src=og, dest=og).pronunciation.lower()
-                        result = text[i]
-                        while word != pronun_list[k].lower():
-                            i += 1
-                            result = result + text[i]
-                            word = translator.translate(result, src=og, dest=og).pronunciation.lower()
-                            word = re.sub("\W+", '', word).lower()
-                        dic[result] = pronun_list[k] + " | " + translate(result, og, "en").text
-                        k += 1
-                        i += 1
-                return dic
+	                if translator.translate(text[i], src=og, dest=og).pronunciation.lower() == pronun_list[k].lower():
+	                        dic[text[i]] = pronun_list[k] + " | " + translate(text[i], og, "en").text
+	                        k += 1
+	                        i += 1
+	                else:
+	                        word = translator.translate(text[i], src=og, dest=og).pronunciation.lower()
+	                        result = text[i]
+	                        while word != pronun_list[k].lower():
+	                            i += 1
+	                            result = result + text[i]
+	                            word = translator.translate(result, src=og, dest=og).pronunciation.lower()
+	                            word = re.sub("\W+", '', word).lower()
+	                        dic[result] = pronun_list[k] + " | " + translate(result, og, "en").text
+	                        k += 1
+	                        i += 1
+	                return dic
 	except Exception as e:
 		return str(e)
 
 def clean_text(text):
 	return text.replace('\n', '').strip()
 
-if __name__ == '__main__':
-    # text = parse_img(img="image1.jpg", lang="chi_sim")
-    text = parse_img("test_img/img3.jpg", "chi_sim")
-    print(generate_vocab(text, "zh-tw"))
-    # print(translate(text, "zh-tw", "en"))
-    print(translator.translate(text, src="zh-tw", dest="en").text)
+def main(file, lang, trans_lang):
+    text = parse_img(file, lang)
+    vocab_dict = generate_vocab(text, trans_lang)
+    translated = translate(text, trans_lang, "en")
+    return (vocab_dict, translated)
