050e0a923b86233a07b97911a14d3b8cc7c46684
Shayla Luong <nganluong1021@gmail.com>
2019-01-12 22:47:17 -0800

    trans_lang file for mapping of languages input

---
+++ trans_lang.py
@@ -0,0 +1,12 @@
+# tesseract [0] -------- googletrans [1]
+LANGUAGES = {
+    "English" : ["eng",'en'],
+    "Spanish" : ["spa",'es'],
+    "German" : ["deu",'de'],
+    "French" : ["fra",'fr'],
+    "Japanese" : ["jpn",'ja'],
+    "Korean" : ["kor",'ko'],
+    "Chinese (simplified)" : ["chi_sim",'zh-cn'],
+    "Chinese (traditional)" : ["chi_tra",'zh-tw'],
+    "Vietnamese" : ["vie",'vi']
+}
